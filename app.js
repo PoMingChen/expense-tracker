40d5c3677cdeaeea0f5528e7830ad2d4e5832a6d
陳柏銘 <alexchen000003@gmail.com>
2024-10-11 12:02:19 +0800

    Build the flash message as reminder when the user register successfully)

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // Load the express module
 const express = require('express');
-
+const flash = require('connect-flash')
+const session = require('express-session')
 const { engine } = require('express-handlebars')
 const port = 3000;
 // Initialize the app
 const app = express();
 const methodOverride = require('method-override')
 const router = require('./routes')
+const messageHandler = require('./middlewares/message-handler')
+const errorHandler = require('./middlewares/error-handler')
 
 app.engine('.hbs',
   engine({
@@ -22,17 +25,32 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
+console.log('env', process.env.NODE_ENV)
+console.log('env', process.env.SESSION_SECRET)
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+
 
 // Define a route for the root URL
 // app.get('/', (req, res) => {
 //   res.send('Hello World');
 // });
 
-
+app.use(messageHandler)
 app.use(router) // 將 request 導入路由器(思考要放在 app.get('/' ,...) 之前還是之後？) 甚至之後 app.get('/'） 要直接 redirect 到 register ？
 app.get('/', (req, res) => {
   res.redirect('/register')
 })
+app.use(errorHandler)
 
 // Start the server and listen on port 3000
 app.listen(port, () => {
---
+++ middlewares/error-handler.js
@@ -0,0 +1,14 @@
+module.exports = (error, req, res, next) => {
+  console.error(error); // Log the error to the console.
+  console.log(req.flash); // Check the current flash messages.
+
+  // Set an error flash message to display to the user, defaulting if not provided.
+  req.flash('error', error.errorMessage || '處理失敗:(');
+
+  res.redirect('back'); // Redirect the user back to the previous page.
+
+  // The purpose of next(error) is to pass the error to the next error-handling middleware.
+  // If there are no other error-handling middleware or routes, 
+  // Express will use the default error-handling mechanism to respond to the error.
+  next(error);
+};
