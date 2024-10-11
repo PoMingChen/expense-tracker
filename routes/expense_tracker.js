99637208f7947c4bd383719b7cc8f049a6839d50
陳柏銘 <alexchen000003@gmail.com>
2024-10-11 14:53:22 +0800

    Add the expense_tracker.js to the routes/index.js

--- routes/expense_tracker.js
+++ routes/expense_tracker.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const expense_tracker = require('./expense_tracker')
 
-router.use('/expense_tracker', expense_tracker)
+// don't load the file itself
+// const expense_tracker = require('./expense_tracker.js')
+// router.use('/expense_tracker', expense_tracker)
 
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index') // renders the index.hbs file
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router()
 
 const root = require('./root.js')
 const users = require('./users.js')
+const expense_tracker = require('./expense_tracker.js')
+const authHandler = require('../middlewares/auth-handler.js')
 
 router.use('/', root) // makes all root routes accessible at the base level
+router.use('/expense_tracker', authHandler, expense_tracker)
 router.use('/users', users)
 
 module.exports = router
